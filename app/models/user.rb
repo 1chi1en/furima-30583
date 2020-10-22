6b7717f3c1d0df91ccfcda9cba06067e9d6d05d2
1chi1en <naokkmt1234@gmail.com>
2020-10-22 12:30:23 +0900

    各バリデーションOK

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ApplicationRecord
+  PASSWORD = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze #英数字混合
+  NAME = /\A[ぁ-んァ-ン一-龥]/.freeze #かな全角カナ漢字
+  KATAKANA = /\A[ァ-ヶー－]+\z/.freeze #全角カナ
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :kana_last_name, presence: true
-  validates :kana_first_name, presence: true
+  validates :last_name, presence: true, format: { with: NAME }
+  validates :first_name, presence: true, format: { with: NAME }
+  validates :kana_last_name, presence: true, format: { with: KATAKANA }
+  validates :kana_first_name, presence: true, format: { with: KATAKANA }
   validates :birth, presence: true
+  validates_format_of :password, with: PASSWORD
 
 end
