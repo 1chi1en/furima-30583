ccf86deeb8f5a4ca5c379c74e86166eeacdd5984
1chi1en <naokkmt1234@gmail.com>
2020-10-23 18:44:10 +0900

    プルダウンメニューの選択肢実装OK

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,11 @@
 class ItemsController < ApplicationController
-  #ルーティングや接続確認の際にトップページに飛ばされるのが面倒なのでauthenticate_user!を一時的にオフ
+  #ルーティングや接続確認の際にトップページに飛ばされると困るのでauthenticate_user!を一時的にオフ
   #before_action :authenticate_user!, except: [:index, :show]
-
   def index
   end
 
   def new
+    @item = Item.new
   end
 
   def create
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to_active_hash :category
+    belongs_to_active_hash :condition
+    belongs_to_active_hash :pref
+    belongs_to_active_hash :shipping_day
+    belongs_to_active_hash :shipping
+
   with_options presence: true do
     validates :title
     validates :detail
