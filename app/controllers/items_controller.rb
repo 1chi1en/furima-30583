bdda695a5349b36b5ff95388b0fa60f6a76cbeb6
1chi1en <naokkmt1234@gmail.com>
2020-10-23 15:55:31 +0900

    itemモデルにバリデーション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  #ルーティングや接続確認の際にトップページに飛ばされるのが面倒なのでauthenticate_user!を一時的にオフ
   #before_action :authenticate_user!, except: [:index, :show]
 
   def index
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :title
+    validates :detail
+    validates :category_id
+    validates :condition_id
+    validates :shipping_id
+    validates :pref_id
+    validates :shipping_day_id
+    validates :price
+  end
 end
