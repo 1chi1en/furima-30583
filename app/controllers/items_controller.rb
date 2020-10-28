aa0a38ee689e0a6edb693897c8fd3b4429a94698
1chi1en <naokkmt1234@gmail.com>
2020-10-28 20:15:35 +0900

    itemsコントローラーの記述をまとめた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :find, only: [:show, :edit, :update]
+
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -18,20 +20,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless user_signed_in? && current_user.id == @item.user_id
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.save
+    if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
@@ -39,6 +37,10 @@ class ItemsController < ApplicationController
   end
 
   private
+
+  def find
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:image, :title, :detail, :price, :category_id, :condition_id, :shipping_id, :pref_id, :shipping_day_id).merge(user_id: current_user.id)
