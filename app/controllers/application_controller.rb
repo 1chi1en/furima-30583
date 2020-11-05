f65999cd42400dfbdf792b3426dbdafc2b1ab14d
1chi1en <naokkmt1234@gmail.com>
2020-11-05 11:53:50 +0900

    Basic認証ON

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  #処理確認のたびにユーザー/PWを求められるのが面倒なのでBasic認証を一時的にオフ
-  #before_action :basic_auth
+  before_action :basic_auth
   before_action :configure_parmitted_parameters, if: :devise_controller?
 
   private
@@ -9,9 +8,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :kana_last_name, :kana_first_name, :birth])
   end
 
-  #def basic_auth
-  #  authenticate_or_request_with_http_basic do |username, password|
-  #    username == 'admin' && password == 'abcd1234'
-  #  end
-  #end
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == 'abcd1234'
+    end
+  end
 end
